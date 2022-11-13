fbc7e20869de929a5ed17a20b8d645943543d155
Peggy8422 <pig2222pig222@gmail.com>
2022-11-13 14:33:44 +0800

    Fix defaultChecked of radio input

--- src/components/Stepform/StepTwo.js
+++ src/components/Stepform/StepTwo.js
@@ -18,10 +18,10 @@ function RadioInfo({text, price, period}) {
 }
 
 
-function RadioGroup({id, children}) {
+function RadioGroup({id, children, ischecked = true}) {
   return (
     <label htmlFor={id} className={styles.radioGroup + " col col-12"}>
-      <input className={styles.inputRadio} id={id} type="radio" name="shipping" defaultChecked/>
+      <input className={styles.inputRadio} id={id} type="radio" name="shipping" defaultChecked={ischecked} />
       {children}
     </label>
   );
@@ -33,8 +33,8 @@ function StepTwoContent() {
       <RadioGroup id="shipping-standard">
         <RadioInfo text="運送標準" price="免費" period="約 3~7 個工作天" />
       </RadioGroup>
-      <RadioGroup id="shipping-dhl">
-        <RadioInfo text="DHL貨運" price="500" period="48 小時內送達" />
+      <RadioGroup id="shipping-dhl" ischecked={false}>
+        <RadioInfo text="DHL貨運" price="500" period="48 小時內送達" ischecked={false} />
       </RadioGroup>
     </section>
   );
